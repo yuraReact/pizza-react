3f6c5b02a8aaad7e2e7fb6ba715d84d40fd6dc93
Yura <vaskivdev2004@gmail.com>
2024-04-18 22:24:50 +0300

    made components and ui

---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+
+function Categories() {
+    return (
+        <div class="categories">
+            <ul>
+                <li class="active">Всі</li>
+                <li>М'ясні</li>
+                <li>Вегетаріанські</li>
+                <li>Гриль</li>
+                <li>Гострі</li>
+                <li>Закриті</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
